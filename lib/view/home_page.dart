a6fb14a5ea94218e8d6c0085009d862ad4822311
Albenga96 <albertoferroni.af@gmail.com>
2021-11-30 14:13:53 +0100

    Refactored code

--- lib/view/home_page.dart
+++ lib/view/home_page.dart
@@ -1,8 +1,4 @@
-import 'dart:io';
-
-import 'package:firebase_storage/firebase_storage.dart';
 import 'package:flutter/material.dart';
-import 'package:image_picker/image_picker.dart';
 import 'package:photo_up/bloc/cubit/photo_cubit.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
